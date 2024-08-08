364836f744559ddf3ed0a35781bc666a2ca9c550
Ayush Rawat <ayushrawat220804@gmail.com>
2024-08-08 17:28:04 +0530

    scripting tutorials

---
+++ learn-batch-scripting/open-url-01.bat
@@ -0,0 +1,9 @@
+@echo off 
+Title Open URL
+echo batch file to open daily urls!!
+pause
+start msedge.exe
+start www.monkeytype.com
+start www.github.com
+start www.youtube.com
+start www.discord.com/channels/
