fe04caaaadfe21480cf444ceff74a4211785ce24
Ayush Rawat <ayushrawat220804@gmail.com>
2024-08-08 17:47:55 +0530

    tutorial

---
+++ learn-batch-scripting/open-url-incognito-02.bat
@@ -0,0 +1,13 @@
+@echo off 
+
+Title Open URL Incognito
+
+echo batch script to open daily urls in incognito mode
+
+pause
+
+:: Double Quotes "": Used to handle spaces in paths or arguments.
+:: Double Dashes --: Used for long-form options or flags in command-line commands.
+
+start msedge.exe --inprivate https://www.monkeytype.com
+
